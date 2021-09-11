111d8cd4a81567fd97ab8094be61f0b67508528a
Parker <hak891@gmail.com>
2021-09-11 15:13:12 +0900

    ReviewDemo0.6

--- src/ReviewDemo01.java
+++ src/ReviewDemo01.java
@@ -20,14 +20,16 @@ public class ReviewDemo01 {
                 break; //첫번째 나누어 떨어질 때 break!
             }
             else
-                //ystem.out.print(i + " ");
+                //System.out.print(i + " ");
 
             i++;
         }
 
-        if(isPrime) // isPrime 변수 값이 true 면
-            System.out.println(number + "는 소수");
-        else
-            System.out.println(number + "는 소수가 아님");
+        System.out.println(number + (isPrime ? "는 소수": "는 소수가 아님"));
+
+        //if(isPrime) // isPrime 변수 값이 true 면
+        //    System.out.println(number + "는 소수");
+        //else
+        //    System.out.println(number + "는 소수가 아님");
     }
 }
