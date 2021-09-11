1350d72f1d07e31b9be037309a5602b20a7246a4
Parker <hak891@gmail.com>
2021-09-11 14:47:20 +0900

    ReviewDemo0.1

---
+++ src/ReviewDemo01.java
@@ -0,0 +1,24 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
+public class ReviewDemo01 {
+    public static void main(String[] args) {
+        // 소수 : 1과 자신 외에는 나누어 떨어지지 않는 수
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int cnt = 0;
+
+        for (int i = 1; i <= number; i ++)
+        {
+            if(number % i == 0)
+                cnt++;
+        }
+
+        if(cnt == 2)
+            System.out.println(number + "는 소수");
+        else
+            System.out.println(number + "는 소수가 아님");
+    }
+}
